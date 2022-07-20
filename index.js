bb374b4a5c7d6c0e66bf7e70917eb58212c59104
dillomn <dillonmbrooks@gmail.com>
2022-07-20 17:49:34 +1000

    Fixed the Crashing issue if the role is deleted

--- index.js
+++ index.js
@@ -14,25 +14,24 @@ bot.on("ready", async () => {
     })
 })
 
-bot.on('voiceStateUpdate', (oldState, newState ) => { // This activates when the bot detects a user joining a vc
-    let vcRole = newState.member.guild.roles.cache.find(role => role.name === "vc"); // Assign variable "vcRole" to a roll named "vc"
-    if (!vcRole) { // If the role doesnt exist run the following 
-        newState.guild.roles.create({ // Creates a role named "vc"
+bot.on('voiceStateUpdate', async (oldState, newState) => {
+    let vcRole = newState.member.guild.roles.cache.find(role => role.name === "vc");
+
+    if (!vcRole) {
+        vcRole = await newState.guild.roles.create({
             data: {
               name: 'vc',
               color: 'BLUE',
             },
             reason: 'The role for vc doesnt exist',
-          })
-            .then(console.log)
-            .catch(console.error);
+          });
     }
-    if (oldState.channel === null && newState.channel !== null) { // If a user joins vc give them vcRole
+    if (oldState.channel === null && newState.channel !== null) { 
         console.log("A user just joined vc !")
-        newState.member.roles.add(vcRole)
+        await newState.member.roles.add(vcRole)
     } else {
-        console.log("A user just left the vc !") // If a user leaves remove their vc role
-        newState.member.roles.remove(vcRole)   
+        console.log("A user just left the vc !")
+        await newState.member.roles.remove(vcRole)   
     }
 });
 
